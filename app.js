a96e6f3fc105ad162d120f6e3155e19c7de39a75
Vicktoriya26 <vkoval4004@gmail.com>
2026-09-19 12:21:17 +0300

    додали товари

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const SUPABASE_URL = "https://jqlcvgzivisddkmijqzs.supabase.co";
 const SUPABASE_ANON_KEY = 'sb_publishable_6Rx3ZzmoLQ9JSCdS2Y0c0g_EXM1eQP8';
 
+let products = [];
+let cart = [];
+const productsContainer = document.querySelector("#productsGrid");
+
 async function fetchData() {
   const response = await fetch(`${SUPABASE_URL}/rest/v1/products`, {
     headers: {
@@ -10,9 +14,39 @@ async function fetchData() {
   });
 
   const data = await response.json();
-  console.log('Fetched data:', data);
-
-  return data;
+  console.log(data);
+  products = data;
+  displayProducts(products)
 }
 
-fetchData()
+function createProductCard(product) {
+  return `
+      <div class="card" style="width: 18rem;">
+          <img src="img/${product.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${product.title}</h5>
+            <p class="card-text">$${product.price}</p>
+            <button type="button" class="btn btn-warning">
+            <i class="bi bi-cart-plus"></i> В кошик </button>
+          </div>
+      </div>
+    `
+
+}
+
+
+function displayProducts(products) {
+    productsContainer.innerHTML = "";
+    products.forEach(product => {
+      const productCard = createProductCard(product);
+      productsContainer.innerHTML += productCard;
+
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+
+})
+
